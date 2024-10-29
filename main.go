20e13f14db1de95533fd9d50c0d8a3a277339407
kimkyungsub <cyon256@icloud.com>
2024-10-30 07:12:07 +0900

    removeDuplicates

--- main.go
+++ main.go
@@ -3,5 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	nums := []int{
+		1,
+		3,
+		2,
+		5,
+		2,
+	}
+
+	k := removeDuplicates(nums);
+	fmt.Println(k);
+}
+
+func removeDuplicates(nums []int) int {
+	prev := 0
+
+	for _, num := range nums {
+		if nums[prev] != num {
+			prev++
+			nums[prev] = num
+		}
+	}
+	return prev + 1
 }
